365c3c1f96b39ca534e79ed48135c5a7a7f77377
XuBingcan <xubingcan@klec.com.cn>
2019-07-02 20:40:22 +0800

    pcc-go fib

--- pcc-go/ch2.1.1-basic_recursive.go
+++ pcc-go/ch2.1.1-basic_recursive.go
@@ -6,3 +6,25 @@ func fact(n int) int {
 	}
 	return n * fact(n-1)
 }
+
+func fib(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fib(n-1) + fib(n-2)
+}
+
+const MAX_N = 100
+
+var memo [MAX_N]int
+
+func fibm(n int) int {
+	if n <= 1 {
+		return n
+	}
+	if memo[n] != 0 {
+		return memo[n]
+	}
+	memo[n] = fibm(n-1) + fibm(n-2)
+	return memo[n]
+}
--- pcc-go/ch2.1.1-basic_recursive_test.go
+++ pcc-go/ch2.1.1-basic_recursive_test.go
@@ -1,6 +1,14 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
+
+func TestFact0(t *testing.T) {
+	if 1 != fact(0) {
+		t.Error("fact 0 not success")
+	}
+}
 
 var ValidTable = map[int]int{
 	1: 1,
@@ -9,16 +17,49 @@ var ValidTable = map[int]int{
 	5: 120,
 }
 
-func TestFact0(t *testing.T) {
-	if 1 != fact(0) {
-		t.Errorf("fact 0 not success")
-	}
-}
-
 func TestFactNormal(t *testing.T) {
 	for k, v := range ValidTable {
 		if v != fact(k) {
 			t.Errorf("fact(%v) Expected(%v) but Got(%v)", k, v, fact(k))
 		}
 	}
+}
+
+func TestFib0(t *testing.T) {
+	if 0 != fib(0) {
+		t.Error("fib 0 not success")
+	}
+}
+
+func TestFib1(t *testing.T) {
+	if 1 != fib(1) {
+		t.Error("fib 0 not success")
+	}
+}
+
+var FibValidTable = map[int]int{
+	0: 0,
+	1: 1,
+	2: 1,
+	3: 2,
+	4: 3,
+	5: 5,
+	6: 8,
+	7: 13,
+}
+
+func TestFibNormal(t *testing.T) {
+	for k, v := range FibValidTable {
+		if v != fib(k) {
+			t.Errorf("fib(%v) = %v, Expeted %v", k, fib(k), v)
+		}
+	}
+}
+
+func BenchmarkFib(b *testing.B) {
+	fib(30)
+}
+
+func BenchmarkFibm(b *testing.B) {
+	fibm(30)
 }
